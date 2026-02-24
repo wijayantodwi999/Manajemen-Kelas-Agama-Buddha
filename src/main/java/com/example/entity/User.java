2adbf456053aecdf4c3275a33ba91b915e6782ef
wijayantodwi999 <wijayantodwi999@gmail.com>
2026-02-24 08:44:01 +0700

    Create User entity class

---
+++ src/main/java/com/example/entity/User.java
@@ -0,0 +1,70 @@
+package com.example.entity;
+
+import java.time.LocalDateTime;
+
+public class User {
+    private Long id;
+    private String nama_lengkap;
+    private String username;
+    private String password;
+    private String tahun_ajaran;
+    private String semester;
+    private LocalDateTime created_at;
+
+    // Getters and Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNama_lengkap() {
+        return nama_lengkap;
+    }
+
+    public void setNama_lengkap(String nama_lengkap) {
+        this.nama_lengkap = nama_lengkap;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getTahun_ajaran() {
+        return tahun_ajaran;
+    }
+
+    public void setTahun_ajaran(String tahun_ajaran) {
+        this.tahun_ajaran = tahun_ajaran;
+    }
+
+    public String getSemester() {
+        return semester;
+    }
+
+    public void setSemester(String semester) {
+        this.semester = semester;
+    }
+
+    public LocalDateTime getCreated_at() {
+        return created_at;
+    }
+
+    public void setCreated_at(LocalDateTime created_at) {
+        this.created_at = created_at;
+    }
+}
